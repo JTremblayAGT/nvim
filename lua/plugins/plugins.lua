e6df43c5dee85908507ec4500b2b2df2098164b7
JTremblayAGT <justin.tremblay@agt-group.com>
2025-05-17 23:09:38 -0400

    add surround plugin

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,12 +1,28 @@
 return {
-    {"simonl91/fanuc-karel-diagnostics.nvim"},
-    {"KnoP-01/vim-tp" },
-    {
-        "neovim/nvim-lspconfig",
-        opts = function()
-            local keys = require("lazyvim.plugins.lsp.keymaps").get()
-            keys[#keys + 1] = { "K", false }
-            keys[#keys + 1] = { "H", function() return vim.lsp.buf.hover() end }
+  { "simonl91/fanuc-karel-diagnostics.nvim" },
+  { "KnoP-01/vim-tp" },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      keys[#keys + 1] = { "K", false }
+      keys[#keys + 1] = {
+        "H",
+        function()
+          return vim.lsp.buf.hover()
         end,
-    },
+      }
+    end,
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
 }
+
