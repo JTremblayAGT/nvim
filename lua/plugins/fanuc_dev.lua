4f1f05dc03f7fe648a26d164eb2185710b77e5aa
Justin.Tremblay <Justin.Tremblay@agt-group.com>
2025-05-18 12:45:09 -0400

    add lsp config for fanuctp

--- lua/plugins/fanuc_dev.lua
+++ lua/plugins/fanuc_dev.lua
@@ -8,22 +8,23 @@ return {
   config = function()
     -- Load the TP Auto Format module
     require("fanuc.tp_auto_format").setup()
-    
+
     -- Load the Karel Compile module
     require("fanuc.klcompile").setup()
-    
+
     -- Load the maketp diagnostics module
     require("fanuc.maketp").setup()
 
     -- Load the checktp module
     require("fanuc.checktp").setup()
-    
+
     -- Load the TP navigation module
     require("fanuc.tp_navigation").setup()
-    
+
     -- You can add more custom modules here
     -- require("custom.your_module").setup()
-    
+
     vim.notify("Fanuc Dev Plugin loaded", vim.log.levels.INFO)
   end,
 }
+
